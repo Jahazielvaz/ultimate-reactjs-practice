a11dc877ddd3ef4a5f7d0923b3bd38e27132ad11
Jahaziel <jahaziel.vaz@gmail.com>
2020-02-21 18:01:31 -0700

    Create-react-app project initiated

---
+++ academind-react-complete-guide/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Person from './Person/Person';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1><Person /></h1>
+        <h1><Person /></h1>
+        <h1><Person /></h1>
+      </div>
+    );
+  }
+}
+
+export default App;
