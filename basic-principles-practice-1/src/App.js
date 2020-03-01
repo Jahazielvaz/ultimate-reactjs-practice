ff35393a2b4748cb7827c96674fe4858bd761f00
Jahaziel Vazquez Alvarez <jahazielvazquezalvarez@Jahaziels-MacBook-Pro.local>
2020-02-29 17:32:08 -0700

    state section is complete

--- basic-principles-practice-1/src/App.js
+++ basic-principles-practice-1/src/App.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Practice from './Practice/Practice';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <Practice name="Jazzy" career="Software Engineer">Seek Not To Compete. Seek To Dominate</Practice>
-    </div>
-  );
+class App extends Component {
+  state = {
+    career: "Find out the career",
+    name: "Find out my name",
+    date: "Find out the date"
+  }
+
+  TodaysDate = () => {
+    const dateObj = new Date();
+    const day = dateObj.getDate();
+    const month = dateObj.getMonth();
+    const year = dateObj.getFullYear();
+
+    return `${month}/${day}/${year}`;
+  };
+
+  dataChangeHandler = () => {
+    this.setState({
+      name: "Jazzy",
+      career: "Software Engineer"
+    })
+  }
+
+
+  render(){
+    const buttonOneStyle = {
+      padding: '10px',
+      margin: '10px',
+      background: 'rgba(184, 105, 95)',
+      fontSize: '20px',
+      color: 'white',
+      fontWeight: 'bolder',
+      borderRadius: '10px',
+      boxShadow: '3px 1px 4px 3px rgba(0, 0, 0, .2)',
+      cursor: 'pointer'
+    }
+
+    return (
+      <div className="App">
+        <button style={buttonOneStyle} onClick={this.dataChangeHandler}>Reveal Data</button>
+        <Practice title={this.state.title} name={this.state.name} date={this.state.date} career={this.state.career}>Seek Not To Compete. Seek To Dominate</Practice>
+      </div>
+    );
+  }
 }
 
 export default App;
--- basic-principles-practice-1/src/Practice/Practice.js
+++ basic-principles-practice-1/src/Practice/Practice.js
@@ -2,20 +2,13 @@ import React from 'react';
 import './Practice.css'
 
 const Practice = (props) => {
-  const todaysDate = () => {
-    const dateObj = new Date();
-    const day = dateObj.getDate();
-    const month = dateObj.getMonth();
-    const year = dateObj.getFullYear();
-
-    return `${month}/${day}/${year}`;
-  };
 
   return(
     <div className="Practice">
-      <h1>This is the practice component</h1>
-      <p>Today's date is {todaysDate()}</p>
-      <p>My name is {props.name} and I'm a {props.career}</p>
+      <button className="practice-button">Reveal Date</button>
+      <h1>{props.title}</h1>
+      <p>Today's date is: {props.date}</p>
+      <p>My name is: {props.name} and I'm a: {props.career}</p>
       <p>{props.children}</p>
     </div>
   )
