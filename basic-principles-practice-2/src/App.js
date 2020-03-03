1a150cbfd0107a252042feec059fb0ce558047cf
Jahaziel Vazquez Alvarez <imaginetech@Jahaziels-MacBook-Pro-2.local>
2020-03-03 00:00:48 -0700

    toggle is now complete

--- basic-principles-practice-2/src/App.js
+++ basic-principles-practice-2/src/App.js
@@ -6,17 +6,32 @@ class App extends Component {
   state = {
     users: [
     {id: 243, name: "Yuna", last: "Ito"},
-    {id: 423, name: "Boa", last: "Kwon"},
+    {id: 423, name: "Boa ", last: "Kwon"},
     {id: 654, name: "Jahaziel", last: "Vazquez"}
-  ]}
+  ],
+  display: false
+}
+
+  toggler = () => {
+    const displayToggle = this.state.display;
+
+    this.setState({
+      display: !displayToggle
+    })
+  }
 
   render(){
     return (
       <div className="App">
         <section id="users-container">
-          <Users name={this.state.users[0].name} last={this.state.users[0].last} />
-          <Users name={this.state.users[1].name} last={this.state.users[1].last} />
-          <Users name={this.state.users[2].name} last={this.state.users[2].last} />
+          <button onClick={this.toggler}>Toggle Users</button>
+          {this.state.display ?
+            <div>
+            <Users name={this.state.users[0].name} last={this.state.users[0].last} />
+            <Users name={this.state.users[1].name} last={this.state.users[1].last} />
+            <Users name={this.state.users[2].name} last={this.state.users[2].last} />
+            </div>
+             : null }
         </section>
       </div>
     );
